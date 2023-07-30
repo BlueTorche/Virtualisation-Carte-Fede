78da69518386d750fe2cd9a19cd8236fcf6edd7c
BlueTorche <dubrulle_kevin@hotmail.be>
2023-07-30 19:52:03 +0200

    base

---
+++ index.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+phpinfo();
+require_once 'phpqrcode/qrlib.php';
+QRcode::png('some othertext 1234');
+?>
+</body>
+</html>
